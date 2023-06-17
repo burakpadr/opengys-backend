cb57214530ce45c5a0a270a7ea7b8888290939d7
burakpadr <b.padir99@gmail.com>
2023-06-18 01:12:19 +0300

    Added docker-compose file and application yml files

---
+++ src/test/java/com/abctech/gys/GysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abctech.gys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
