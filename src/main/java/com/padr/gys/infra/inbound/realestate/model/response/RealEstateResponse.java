404bffadbdc0b1d571a0b01a5d500c2ad77e3c75
burakpadr <b.padir99@gmail.com>
2023-09-02 23:59:29 +0300

    Refactored RealEstateResponse

--- src/main/java/com/padr/gys/infra/inbound/realestate/model/response/RealEstateResponse.java
+++ src/main/java/com/padr/gys/infra/inbound/realestate/model/response/RealEstateResponse.java
@@ -1,10 +1,6 @@
 package com.padr.gys.infra.inbound.realestate.model.response;
 
-import java.util.Objects;
-
 import com.padr.gys.domain.realestate.entity.RealEstate;
-import com.padr.gys.domain.status.constant.MainStatus;
-import com.padr.gys.domain.status.constant.SubStatus;
 
 import lombok.Builder;
 import lombok.Data;
@@ -15,22 +11,19 @@ public class RealEstateResponse {
 
     private Long id;
     private String no;
-    private Long categoryId;
-    private Long subCategoryId;
-    private MainStatus mainStatus;
-    private SubStatus subStatus;
-    private AddressResponse address;
+    private String mainStatus;
+    private String subStatus;
+    private String districtName;
+    private String cityName;
 
     public static RealEstateResponse of(RealEstate realEstate) {
         return RealEstateResponse.builder()
                 .id(realEstate.getId())
                 .no(realEstate.getNo())
-                .categoryId(realEstate.getCategory().getId())
-                .subCategoryId(
-                        Objects.nonNull(realEstate.getSubCategory()) ? realEstate.getSubCategory().getId() : null)
-                .mainStatus(realEstate.getMainStatus())
-                .subStatus(realEstate.getSubStatus())
-                .address(AddressResponse.of(realEstate.getAddress()))
+                .mainStatus(realEstate.getMainStatus().getValue())
+                .subStatus(realEstate.getSubStatus().getValue())
+                .districtName(realEstate.getAddress().getDistrictName())
+                .cityName(realEstate.getAddress().getCityName())
                 .build();
     }
 }
