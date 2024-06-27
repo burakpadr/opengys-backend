d900edc7641455b80e310ff7cb97d61906b5694f
burakpadr <b.padir99@gmail.com>
2024-06-27 22:17:47 +0300

    Email templates translated into english

--- src/main/java/com/padr/gys/domain/carrier/constant/EmailTemplate.java
+++ src/main/java/com/padr/gys/domain/carrier/constant/EmailTemplate.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum EmailTemplate {
     
-    PASSWORD_RESET_OTP("reset-password", "OpenGYS Parola Yenileme Kodu"),
-    STAFF_ACCOUNT_HAS_BEEN_CREATED("staff-account-has-been-created", "OpenGYS Hesabı"),
-    TENANT_ACCOUNT_HAS_BEEN_CREATED("tenant-account-has-been-created", "OpenGYS Kiracı Hesabı");
+    PASSWORD_RESET_OTP("reset-password", "OpenGYS Reset Password Code"),
+    STAFF_ACCOUNT_HAS_BEEN_CREATED("staff-account-has-been-created", "OpenGYS Staff Account"),
+    TENANT_ACCOUNT_HAS_BEEN_CREATED("tenant-account-has-been-created", "OpenGYS Tenant Account");
 
     private final String templateName;
     private final String subject;
